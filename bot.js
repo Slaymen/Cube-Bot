6ce25a8c60fb07c1dd9ef85c80d760156e7c9040
Slaymen <slaymen.hazzouri@gmail.com>
2018-02-03 13:17:20 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.setGame(`http://nixbot.tk/ | currently on ${client.guilds.size} servers`);
 });
 
 client.on("guildCreate", guild => {
